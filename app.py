20817edeb16ecaa328c8cd413156a3674ed5bd6e
Cleber Silva <cleber_pesilva@yahoo.com.br>
2024-04-13 18:13:29 -0300

    feat: small 2.0

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
+import time
 from selenium import webdriver
-driver = webdriver.Chrome()
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+
+driver = webdriver.Chrome( options=chrome_options)
+
 driver.get("https://medium.com")
 print(driver.page_source)
 
 driver.quit()
 print("Finished!")
+
+time.sleep(10)
